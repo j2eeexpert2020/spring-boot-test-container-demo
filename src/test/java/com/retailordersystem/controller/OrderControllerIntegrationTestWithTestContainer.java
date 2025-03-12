86a52eda7c037d46404a343bed658c8b27616b76
j2eeexpert2015 <j2eeexpert2020@gmail.com>
2025-03-12 13:18:21 +0530

    Pipeline fix

--- src/test/java/com/retailordersystem/controller/OrderControllerIntegrationTestWithTestContainer.java
+++ src/test/java/com/retailordersystem/controller/OrderControllerIntegrationTestWithTestContainer.java
@@ -8,17 +8,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 import org.testcontainers.utility.DockerImageName;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,6 +39,10 @@ import com.retailordersystem.repository.OrderRepository;
 
 public class OrderControllerIntegrationTestWithTestContainer {
 	
+
+    private static final Integer TIMEOUT = 120;
+    private static final Logger logger = LoggerFactory.getLogger(OrderControllerIntegrationTestWithTestContainer.class);
+	
 	@Autowired
     private MockMvc mockMvc;
 
@@ -41,8 +53,33 @@ public class OrderControllerIntegrationTestWithTestContainer {
     private ObjectMapper objectMapper;
     
     @Container
-    static PostgreSQLContainer<?> postgreSQLContainer = 
+    static PostgreSQLContainer<?> postgres = 
             new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    
+    @BeforeAll
+    static void startContainers() {
+
+        logger.info("Starting PostgreSQL ");
+        postgres.start();
+
+        logger.info("PostgreSQL started. Waiting for full initialization...");
+
+        // Ensure PostgreSQL is running
+        Awaitility.await().atMost(Duration.ofSeconds(TIMEOUT)).until(postgres::isRunning);
+        logger.info("PostgreSQL is up and running!");
+
+       
+
+        logger.info("Test environment setup complete! Ready to execute tests.");
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+    }
 
 
     @Test
